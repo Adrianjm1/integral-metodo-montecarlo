2179e4653a543e7f38a8a18343d9e623299a0a8c
unknown <adrianmolina39@gmail.com>
2022-07-02 17:07:29 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,44 +1,64 @@
 
 
-//Definimos los valores
+
+
+const simular = () => {
+
+
+ //Definimos los valores
 // a y b son los limites de la integral
-const a = 2;
-const b = 3;
+const a = parseInt(document.getElementById("limitea").value)  ;
+const b = parseInt(document.getElementById("limiteb").value) ;
+
+
 
 //Resolvemos integral propuesta por el metodo normal
 const integral =   (Math.pow(b,3) +  Math.pow(b,2)) - ((Math.pow(a,3) +  Math.pow(a,2)))
 
 //M es la cota superior
-const m = 30;
+const m = parseInt(document.getElementById("cotas").value) ;
 
 
 
 // n, numero de simulaciones
-let n = 1000;
-
+let n = parseInt(document.getElementById("numeroDeSimulaciones").value)  ;
 
 let punto =0;
 
-for (let i = 0; i < n; i++) {
+
+
+    for (let i = 0; i < n; i++) {
+        
+        //Calculando X y Y
+        let x = a + (Math.random()) * (b-a);
+        let y = (Math.random()) * m
+        let resultx = 3 * Math.pow(x,2) + 2 * x;
+        let err = Math.abs(((integral - resultx)/integral).toFixed(5));
     
-    //Calculando X y Y
-    let x = a + (Math.random()) * (b-a);
-    let y = (Math.random()) * m
-    let resultx = 3 * Math.pow(x,2) + 2 * x;
-
-
-
-    if (y <= resultx) {
-        punto++;
+        document.getElementById("results").insertRow(-1).innerHTML = `<tr ><th scope="row">${i+1}</th><td >${y.toFixed(4)}</td> <td >${resultx.toFixed(4)}</td> <td >${err.toFixed(4)}</td>  <td style="color:${y <= resultx ? `green`: `red`} ; font-weight:bold">${y <= resultx ? 'Exito': 'Fracaso'}</td></tr>`
+        if (y <= resultx) {
+            punto++;
+        }
+        
     }
     
-}
+    
+    let monteCarlo = (punto/n) * (b-a) * m ;
+    let error = Math.abs(((integral - monteCarlo)/integral).toFixed(8));
 
+    document.getElementById("integral").value = integral;
+    document.getElementById("monte").value = monteCarlo.toFixed(4);
+    document.getElementById("error").value = error.toFixed(4);
+    document.getElementById("exito").value = punto;
+    document.getElementById("fracaso").value = n- punto;
+    document.getElementById("todosLosResultados").style = "display:block";
 
-let monteCarlo = (punto/n) * (b-a) * m ;
-let error = Math.abs(((integral - monteCarlo)/integral).toFixed(8));
+    
+    
+    console.log('Integral propuesta: ' + integral +'\n' + 'Integral calculada: ' + monteCarlo + '\n' + 'Error: ' + error);
+    
 
-console.log('Integral propuesta: ' + integral +'\n' + 'Integral calculada: ' + monteCarlo + '\n' + 'Error: ' + error);
+}    
 
 
 
